3cf7ca2a5ff9d23d926f8f52e1cdad6d7813e22d
Aaron Jordan <aabjordan88@gmail.com>
2023-01-10 13:34:52 -0500

    2nd commit

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ var apiKey = "de4a23ba34d70e491d72d09ec39f7beb";
 var lat;
 var lon;
 var city;
-
 let input = document.getElementById("query");
 let search = document.getElementById("search");
 let query = document.getElementById("query");
 let forecastContainer = document.getElementById("forecast-container");
-
 if (localStorage.getItem("saveCity") === null) {
   localStorage.setItem("saveCity", JSON.stringify([0], [1], [2], [3], [4]));
 }
@@ -28,52 +26,52 @@ async function getCoordinates(event) {
   lon = data[0].lon;
   getWeather();
 }
-
 async function getWeather() {
-  const api_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
+  const api_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
   const response = await fetch(api_url);
   let data = await response.json();
   console.log(data);
-
-  for (let index = 0; index < data.list.length; index += 8) {
+  console.log(data.list[0].main.temp);
+  for (let index = 0; index < 5; index++) {
     console.log("date and time", data.list[index].dt_txt);
     // use to render your daily cards here
-
     const iconImage = document.createElement("img");
     iconImage.src = `https://openweathermap.org/img/wn/${data.list[index].weather[0].icon}@2x.png`;
-
     forecastContainer.appendChild(iconImage);
-
-    const weatherCond = document.getElementById("day1Temp");
-    weatherCond.src = `https://openweathermap.org/img/wn/${data.list[index].weather[0].main.temp}`;
-    forecastContainer.appendChild(weatherCond);
+    let temp1 = document.getElementById("day1Temp");
+    console.log(data.list[index].main.temp);
+    temp1.innerHTML = data.list[index].main.temp;
+    console.log(temp1);
+    forecastContainer.appendChild(temp1);
+    let wind1 = document.getElementById("day1Wind");
+    console.log(data.list[index].wind.speed);
+    wind1.innerHTML = data.list[index].wind.speed;
+    console.log(wind1);
+    forecastContainer.appendChild(wind1);
+    let hum1 = document.getElementById("day1Hum");
+    console.log(data.list[index].main.humidity);
+    hum1.innerHTML = data.list[index].main.humidity;
+    console.log(hum1);
+    forecastContainer.appendChild(hum1);
   }
 }
-
 // document.getElementById('conditions') = `https://openweathermap.org/img/wn/${data.list[index].weather[0].description}@2x.png`;
 // // conditions.src = `https://openweathermap.org/img/wn/${data.list[index].weather[0].main}@2x.png`;
-
 // forecastContainer.appendChild(conditions);
-
 // localStorage.setItem("saveWeather", JSON.stringify(data));
 saveInput();
 query.value = "";
 // storageOutput()
-
 function saveInput() {
   city = document.getElementById("query").value;
   var outData = JSON.parse(localStorage.getItem("saveCity"));
   outData.push(city);
   localStorage.setItem("saveCity", JSON.stringify(outData));
 }
-
 function storageOutput() {
   city = localStorage.getItem("saveCity");
   console.log(city);
   document.getElementById("log").innerHTML = city;
 }
-
 storageOutput();
-
 search.addEventListener("click", getCoordinates);
